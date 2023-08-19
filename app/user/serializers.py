64dd61c87616f67cdaf6530006b610006b1881a7
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-08-19 16:17:50 +0330

    reformat

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -6,6 +6,7 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
--- app/user/views.py
+++ app/user/views.py
@@ -4,8 +4,7 @@ Views for the user API.
 from rest_framework.generics import CreateAPIView
 from .serializers import UserSerializer
 
+
 class CreateUserAPIView(CreateAPIView):
     """Create a new user."""
     serializer_class = UserSerializer
-
-
