7af24389e59f7fdf9e865b4deb15fed2bcb5bfd1
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2023-09-19 09:45:40 +0330

    added static and media configs

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+STATIC_ROOT = BASE_DIR / 'vol/web/static'
+MEDIA_ROOT = BASE_DIR / 'vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
